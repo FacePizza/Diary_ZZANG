06cc94f93f85505115bb8797acf6e79845a495e2
신민호 <68156836+alsh0807@users.noreply.github.com>
2024-06-17 14:48:46 +0900

    프로젝트 시작

---
+++ src/test/java/kr/hs/dgsw/Template/TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.Template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
